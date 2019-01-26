b4a8cb5b91fef2c437a260b8e07f59a26cf30c7a
Paulina Lei <paulina.y.lei@gmail.com>
2019-01-26 00:36:55 +0000

    small changes

--- src/js/story.js
+++ src/js/story.js
@@ -79,8 +79,9 @@ export const stories = [
         'title-explanation-src': '[*]J. A. Santora, E. L. Hazen, I. D. Schroeder, S. J. Bograd, K. M. Sakuma, and J. C. Field. Impacts of ocean climate variability on biodiversityof pelagic forage species in an upwelling ecosystem. Marine EcologyProgress Series, 580:205–220, 2017.'
     },
     {
-        'explanation': 'In 2011, which was a peak cold year*, we see that this caused a decrease in species diversity in 2012. </br> [*]https://ggweather.com/enso/oni.htm',
-        'image': './src/img/2011_2012_compare.png'
+        'explanation': 'In 2011, which was a peak cold year*, we see that this caused a decrease in species diversity in 2012.',
+        'image': './src/img/2011_2012_compare.png',
+        'image-src': '[*]https://ggweather.com/enso/oni.htm'
     },
     {
         'explanation': 'We see a decrease in the diversity of <span style="color:red">Rockfish</span> and an increase for <span style="color:#a65628">Gelatinous</span> species.',
@@ -99,16 +100,18 @@ export const stories = [
         'explanation': 'From 2011 to 2014, we saw that there was an abundance of plankton and euphausiids, which fish tend to feed on.'
     },
     {
-        'explanation': 'Here we see a histogram of the species from 2011 to 2015. Years are read from left to right. Each histogram corresponds to a specific species and is color coordinated with the legend. The histograms are mirrored horizontally except for <span style="color:#f781bf">Krill</span> and <span style="color:#a65628">Gelatinous</span>.',
+        'explanation': 'Here we drew a histogram of the species from 2011 to 2015. Years are read from left to right. Each histogram corresponds to a specific species and is color coordinated with the legend. The histograms are mirrored horizontally except for <span style="color:#f781bf">Krill</span> and <span style="color:#a65628">Gelatinous</span>.',
         'image': './src/img/infographic.png'
     },
     {
         'explanation': '<span style="color:#f781bf">Krill</span> and <span style="color:#a65628">Gelatinous</span> species are shown as fins at the head of the fish. <span style="color:#f781bf">Krill</span> grew exponentially until 2014 and then experienced a drop in 2015. <span style="color:#a65628">Gelatinous</span> was not collected in 2011. However, krill grew exponentially from 2012 and peaked in 2014. Then, it dropped quickly in 2015.',
-        'image': './src/img/gel_krill_infographic.png'
+        'image': './src/img/gel_krill_infographic.png',
+        'image-src': 'Krill image: https://commons.wikimedia.org/wiki/File:Krill666.jpg, Gelatinous image: https://www.flickr.com/photos/jlambus/2348695979'
     },
     {
         'explanation': '<span style="color:#42d4f4">Forage</span> species had low abundance in 2011, a peak cold year, but began to increase in population as sea surface temperatures got warmer. This is seen as a peak in 2014, a warm year.',
-        'image': './src/img/forage.png'
+        'image': './src/img/forage.png',
+        'image-src': 'Forage image: https://en.wikipedia.org/wiki/Pacific_herring#/media/File:Clupea_pallasii_by_OpenCage.jpg'
     },
     {
         'title': 'Warmer ocean temperatures with weak upwelling favor production of forage species and a decrease in krill*.',
@@ -122,8 +125,12 @@ export const stories = [
         'image': './src/img/popOnly_2015.png'
     },
     {
-        'title': 'Changes in the marine ecosystem are also seen in changes in the food web.',
-        'explanation': 'In 2015, we see that there is a higher proportion of gelatinous plankton. This shows a shift in the food web from a system dominated by euphausiids to gelatinous organisms.'
+        'title': 'Changes in the marine ecosystem are also seen in changes in the food web.'
+    },
+    {
+        'explanation': 'In 2015, we see that there is a higher proportion of gelatinous plankton. This shows a shift in the food web from a system dominated by euphausiids to gelatinous organisms.',
+        'image': './src/img/krill_gel_2015.png',
+        'image-src': 'Krill image: https://commons.wikimedia.org/wiki/File:Krill666.jpg, Gelatinous image: https://www.flickr.com/photos/jlambus/2348695979'
     },
     {
         'title': 'In conclusion, we can see that climate change has intense impact on marine ecosystems.',
